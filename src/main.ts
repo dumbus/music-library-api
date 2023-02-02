e2649b719638d25066b35742019747b3f9ac5375
dumbus <dumbusx@gmail.com>
2023-02-03 01:28:55 +0300

    init: init nodejs2022q4-service task

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const PORT = Number(process.env.PORT || 4000);
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  await app.listen(PORT);
 }
 bootstrap();
