734e4bcf55daf48f8a6c5b730219aaba9b634c9e
dumbus <dumbusx@gmail.com>
2023-02-03 22:56:44 +0300

    feat: artist service use database

--- src/artist/artist.module.ts
+++ src/artist/artist.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+
+import { DbModule } from 'src/db/db.module';
 import { ArtistService } from './artist.service';
 import { ArtistController } from './artist.controller';
 
 @Module({
+  imports: [DbModule],
   providers: [ArtistService],
   controllers: [ArtistController],
 })
--- src/artist/artist.service.ts
+++ src/artist/artist.service.ts
@@ -1,16 +1,16 @@
 import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
 import { v4 as uuidv4, validate as uuidValidate } from 'uuid';
 
-import { Artist } from './interfaces/artist.interface';
+import { DbService } from 'src/db/db.service';
 import { CreateArtistDto } from './dto/create-artist.dto';
 import { UpdateArtistDto } from './dto/update-artist.dto';
 
 @Injectable()
 export class ArtistService {
-  private artists: Artist[] = [];
+  constructor(private db: DbService) {}
 
   getAll() {
-    return this.artists;
+    return this.db.artists;
   }
 
   getById(id: string) {
@@ -18,7 +18,7 @@ export class ArtistService {
       throw new HttpException('Invalid artist ID', HttpStatus.BAD_REQUEST);
     }
 
-    const artist = this.artists.find((artist) => artist.id === id);
+    const artist = this.db.artists.find((artist) => artist.id === id);
 
     if (!artist) {
       throw new HttpException('Artist was not found', HttpStatus.NOT_FOUND);
@@ -32,7 +32,7 @@ export class ArtistService {
       throw new HttpException('Invalid artist ID', HttpStatus.BAD_REQUEST);
     }
 
-    const artistIndex = this.artists.findIndex((artist) => artist.id === id);
+    const artistIndex = this.db.artists.findIndex((artist) => artist.id === id);
 
     if (artistIndex === -1) {
       throw new HttpException('Artist was not found', HttpStatus.NOT_FOUND);
@@ -44,7 +44,7 @@ export class ArtistService {
   create(createArtistDto: CreateArtistDto) {
     const artistId = uuidv4();
     const artist = { id: artistId, ...createArtistDto };
-    this.artists.push(artist);
+    this.db.artists.push(artist);
 
     return artist;
   }
@@ -60,7 +60,7 @@ export class ArtistService {
 
   delete(id: string) {
     const artistIndex = this.getIndexById(id);
-    this.artists.splice(artistIndex, 1);
+    this.db.artists.splice(artistIndex, 1);
 
     return null;
   }
