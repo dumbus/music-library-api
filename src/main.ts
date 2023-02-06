4cfe4031dc8e09edfa3d45f40ff40a9b2834468e
dumbus <dumbusx@gmail.com>
2023-02-06 03:08:36 +0300

    feat: add swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 import * as dotenv from 'dotenv';
+import * as yaml from 'js-yaml';
+import { readFile } from 'fs/promises';
 
 import { AppModule } from './app.module';
 
@@ -11,6 +14,12 @@ const PORT = Number(process.env.PORT || 4000);
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
+  const document = yaml.load(
+    await readFile('./doc/api.yaml', { encoding: 'utf-8' }),
+  );
+  SwaggerModule.setup('doc', app, document);
+
   await app.listen(PORT);
 }
 bootstrap();
