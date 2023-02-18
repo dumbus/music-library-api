42a161c4c94c6d0547eed2b87d3e0da3db9438b7
dumbus <dumbusx@gmail.com>
2023-02-18 21:29:09 +0300

    refactor: move typeorm config to a separate file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import * as dotenv from 'dotenv';
+
+import { typeormConfig } from './typeorm.config';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -11,24 +12,6 @@ import { UserModule } from './app/user/user.module';
 import { TrackModule } from './app/track/track.module';
 import { FavoritesModule } from './app/favorites/favorites.module';
 
-import { UserEntity } from './app/user/entities/user.entity';
-import { ArtistEntity } from './app/artist/entities/artist.entity';
-import { AlbumEntity } from './app/album/entities/album.entity';
-import { TrackEntity } from './app/track/entities/track.entity';
-import { FavoriteAlbumEntity } from './app/favorites/entities/favoriteAlbum.entity';
-import { FavoriteArtistEntity } from './app/favorites/entities/favoriteArtist.entity';
-import { FavoriteTrackEntity } from './app/favorites/entities/favoriteTrack.entity';
-
-dotenv.config();
-
-const {
-  POSTGRES_USERNAME,
-  POSTGRES_PASSWORD,
-  POSTGRES_DATABASE,
-  POSTGRES_PORT,
-  POSTGRES_HOST,
-} = process.env;
-
 @Module({
   imports: [
     DbModule,
@@ -37,24 +20,7 @@ const {
     UserModule,
     TrackModule,
     FavoritesModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      username: POSTGRES_USERNAME,
-      password: POSTGRES_PASSWORD,
-      database: POSTGRES_DATABASE,
-      host: POSTGRES_HOST,
-      port: Number(POSTGRES_PORT),
-      entities: [
-        UserEntity,
-        ArtistEntity,
-        AlbumEntity,
-        TrackEntity,
-        FavoriteAlbumEntity,
-        FavoriteArtistEntity,
-        FavoriteTrackEntity,
-      ],
-      synchronize: true,
-    }),
+    TypeOrmModule.forRoot(typeormConfig.options),
   ],
   controllers: [AppController],
   providers: [AppService],
